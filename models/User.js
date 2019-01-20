bf275dd49e9ba17525a41659f5efa0caa039065d
omar-nav <omar.navarro@g.austincc.edu>
2019-01-20 12:08:06 -0600

    create a document for John Doe every time the app runs

--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-  name: String,
-  email: String
+  name: { required: true, 'Let us know you by adding your name!' },
+  email: { required: true,  'Please add your email as well.' }
 })
 // mongoose.model(ModelName)
 const User = mongoose.model("User", UserSchema)
